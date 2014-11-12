64dfe11968221365a2d6bf5781835520dde738b0
Todd Skinner <todd.skinner@gmail.com>
2014-11-11 20:34:16 -0800

    initial commit stringifyJSON.js

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,6 +2,46 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
+
 var stringifyJSON = function(obj) {
-  // your code goes here
+	if(typeof(obj)==="number"){												//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof
+		return String(obj);
+	} else if(typeof(obj) === "undefined"){
+		return null;
+	} else if(typeof(obj)==="function"){
+		return null;
+	} else if(obj === null) {
+		return 'null';
+	} else if(typeof(obj) === "boolean"){
+		return obj ? "true" : "false";
+	} else if(typeof(obj) === "string"){
+		return "\"" + obj + "\"";
+	} else if(Array.isArray(obj) === true && obj.length === 0){					//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray
+		return "\[\]";
+	} else if(Array.isArray(obj) === true && obj.length > 0){					
+		return "\[" + _.map(obj, function(element){				// _.map = function(collection, iterator) { var result = []; _.each(collection, function(item, index, collection){ result.push(iterator(item)); }); return result;  }; 
+			if(typeof(element) === "number"){
+				return element;
+			} else {
+				return stringifyJSON(element);
+			}
+		}) + "\]";
+	} else if(typeof(obj) === "object") {
+		var result = [];
+		Object.keys(obj).forEach(function(key){					//http://bob.yexley.net/iterating-over-javascript-objects/
+			var val = stringifyJSON(obj[key]);
+			if(val !== null) {
+				result.push('"' + key + '":' + val);
+			}
+		})
+		return "{" + result.join(",") + "}"
+	}
 };
+			
+
+
+// 		return "\{" + _.map(obj, function(value, key){
+// 			return stringifyJSON(key) + ":" + stringifyJSON(value);
+// 		}) + "\}";
+// 	}
+// };
